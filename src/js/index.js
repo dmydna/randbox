ca0c82957b8466a66992be78189af43b12335d06
nahuelcerrano <nahuelcerrano21@gmail.com>
2025-04-26 18:35:23 -0300

    Se corrigió un bug de no mostrar el ultimo gift y el spam de click. Tambien se agrego un mensaje con los gifts restantes

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,39 @@ import { gift_img, limitClicks } from "./config.js";
 
 const box = document.querySelector(".box");
 const gift = document.querySelector(".gift");
+const REMAIN_GIFTS = document.querySelector(".remain_gifts");
 
-let clickEvent = 0;
+// Creamos el mensaje final, pero escondido de entrada
+const finalMessage = document.createElement("div");
+finalMessage.textContent = "¡Última sorpresa!";
+finalMessage.style.position = "absolute";
+finalMessage.style.top = "10%";
+finalMessage.style.width = "100%";
+finalMessage.style.textAlign = "center";
+finalMessage.style.fontSize = "1.5rem";
+finalMessage.style.color = "#FF0000";
+finalMessage.style.opacity = "0";
+finalMessage.style.transition = "opacity 0.8s ease";
+document.body.appendChild(finalMessage);
+
+let clickEvent = false;
 let cantRepetciones = new Array(gift_img.length).fill(0);
 let cantClicks = limitClicks;
+
+REMAIN_GIFTS.textContent = cantClicks;
 
 
 box.addEventListener('click', () => {
 
-    box.classList.remove("onload");    
+    box.classList.remove("onload");
+
+    if (cantClicks <= 0) return;
+
+    // Desactivar los clicks por 3 segundos después de hacer click
+    box.style.pointerEvents = "none";
+    setTimeout(() => {
+        box.style.pointerEvents = "auto";
+    }, 3100);
 
     let rand_index =  Math.floor(Math.random() * gift_img.length);
 
@@ -19,19 +43,31 @@ box.addEventListener('click', () => {
     gift.src = `/imgs/${gift_img[rand_index]}.png`;
     
     
-    if (clickEvent == 0){
-        clickEvent=1;
+    if (clickEvent === false){
+        clickEvent = true;
         box.classList.add("tirarBox")
         gift.style.opacity="1";
     }else{
-        clickEvent=0;
+        clickEvent = false;
         box.classList.remove("tirarBox")
     }
     
     cantClicks--;
 
-    if (cantClicks == 0) {
-        window.location.href = "../src/pages/quiz.html"
+    REMAIN_GIFTS.textContent = cantClicks;
+
+    if (cantClicks === 0) {
+        // Mostrar mensaje de última sorpresa
+        finalMessage.style.opacity = "1";
+
+        clearTimeout();
+
+        // Desactivar el botón (eliminar el eventListener)
+        box.style.pointerEvents = "none";
+
+        setTimeout(() => {
+            window.location.href = "../src/pages/quiz.html";
+        }, 3500);
     }
 
     localStorage.setItem("cantRepeticiones", JSON.stringify(cantRepetciones));
