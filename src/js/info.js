560b9832aa56fab293f9eedd2d41d8aa94a2dedb
dmydna <danimaydana9@gmail.com>
2025-05-06 10:53:27 -0300

    agrega pag info

--- src/js/info.js
+++ src/js/info.js
@@ -1,10 +1,13 @@
 
 const randbox = document.querySelector(".randbox");
-const resetBtn = document.querySelector('.resetBtn')
-const plusBtn = document.querySelector('.plusBtn')
+const homeBtn = document.querySelector('.resetBtn')
+const backBtn = document.querySelector('.plusBtn')
 const ui = document.querySelector(".ui")
 const score_container = document.querySelector(".score")
+const socialBtn = document.querySelector(".social")
 
-resetBtn.addEventListener('click', () => { window.location.href = "../index.html" } )
-plusBtn.addEventListener('click', () => { window.location.href = "../quiz.html" } )
+homeBtn.addEventListener('click', () => { window.location.href = "../index.html" } )
+backBtn.addEventListener('click', () => { window.location.href = "../quiz.html" } )
+
+socialBtn.addEventListener('click', ()=>{  window.open( "https://github.com/dmydna/randbox", '_blank')  })
 
